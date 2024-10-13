794ea25ba86dcbe346b1203a22e7acf64a8c504e
Claudenes Renato Soares de Almeida <claudenes.almeida@grupoaguasdobrasil.com.br>
2024-10-13 09:59:48 -0300

    Implementando Cama IoC Injeção de dependencia

---
+++ Ambev/Ambev.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,52 @@
+﻿using Ambev.Application.Interfaces;
+using Ambev.Application.Mappings;
+using Ambev.Application.Services;
+using Ambev.Domain.Interfaces;
+using Ambev.Infra.Data.Context;
+using Ambev.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ambev.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+       
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            ConfigureEntityFrameWork(services);
+            services.AddScoped<IClienteRepository, ClienteRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IFilialRepository, FilialRepository>();
+            services.AddScoped<IVendaRepository, VendaRepository>();
+            services.AddAutoMapper(typeof(DomainToDtoMappingProfile));
+
+            var myhandlers = AppDomain.CurrentDomain.Load("Unico.Application");
+
+            return services;
+        }
+        private static void ConfigureEntityFrameWork(this  IServiceCollection services)
+        {
+            string connectionString = "";
+
+            connectionString = @"Data Source=DESKTOP-OAG94LR\SQLEXPRESS;Initial Catalog=AMBEV;Integrated Security=True;";
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+
+                options.UseSqlServer(connectionString,
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)); options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+        }
+        public static IServiceCollection AddServices(this IServiceCollection services) 
+        {
+            
+            services.AddScoped<IVendaService, VendaService>();
+            services.AddAutoMapper(typeof(DomainToDtoMappingProfile));
+            return services;
+        }
+    }
+    
+}
